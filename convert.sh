21d4c2a47115b095d26a67adfe192aedeb98a2b3
bikash720 <63512238+bikash720@users.noreply.github.com>
2022-11-20 00:31:46 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
  # Change input and output details
 
-output_path="test" # Output Path (Same will be pushed in main also)
+output_path="zalim" # Output Path (Same will be pushed in main also)
 input_url="https://stream.sbpmaza.workers.dev/0:/Sunaguri.mp4" # Input direct file url
 input_extension="mp4" # Extension of file url
 
